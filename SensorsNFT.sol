b416ba8a641eb8f3a49b4022d950b7aba76dd240
Andrej Vrkljan <98988595+Andrej656@users.noreply.github.com>
2022-06-26 22:40:41 +0200

    Create SensorsNFT.sol

---
+++ SensorsNFT.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts@4.6.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.6.0/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts@4.6.0/security/Pausable.sol";
+import "@openzeppelin/contracts@4.6.0/access/Ownable.sol";
+import "@openzeppelin/contracts@4.6.0/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts@4.6.0/utils/Counters.sol";
+
+contract Sensors is ERC721, ERC721Enumerable, Pausable, Ownable, ERC721Burnable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    uint256 public MINT_Price = 0.05 ether;
+    uint256 public MAX_Supply = 2222;
+
+    constructor() ERC721("Sensors", "Sen") {
+
+        _tokenIdCounter.increment();
+
+    }
+
+    function withdraw() public onlyOwner() {
+
+        require(address(this).balance > 0, "Balance is zero");
+        payable (owner()).transfer(address(this).balance);
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://happyMonkeyBaseURI/";
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function safeMint(address to) public payable {
+
+        require(totalSupply() < MAX_Supply,"Can not mint anymore tokens");
+        require(msg.value >= MINT_Price, "Not enough ether sent");
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        whenNotPaused
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
